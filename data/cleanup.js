01b5233c39fe89a5d71e30912927bdd9ce61e9a7
Julia Lind <jl225vf@student.lnu.se>
2025-04-15 22:12:38 +0200

    Fix linting errors

--- data/cleanup.js
+++ data/cleanup.js
@@ -1,8 +1,7 @@
 import fs from 'fs/promises'
 
 const willysOriginal = './willys.jsonl'
-
-let lines = (await fs.readFile(willysOriginal, 'utf8')).split('\n').filter(Boolean)
+const lines = (await fs.readFile(willysOriginal, 'utf8')).split('\n').filter(Boolean)
 const willys = lines.map(line => JSON.parse(line))
 
 const excludeCategory = [
@@ -19,11 +18,15 @@ const excludeCategory = [
   'blommor-och-tradgard',
   'barn',
   'chark',
-  'delikatesschark',
+  'delikatesschark'
 ]
-const clean = []
-const excluded = []
 
+/**
+ * Get the nutrient values from a food item.
+ *
+ * @param {object} item - associative array with food data
+ * @returns {object} - object with nutrient values
+ */
 function getNutrients (item) {
   const nutrients = {}
 
@@ -57,9 +60,17 @@ function getNutrients (item) {
         break
     }
   }
+  return nutrients
 }
 
-async function cleanObj(item) {
+/**
+ * Selects the relevant data from the food item.
+ * Writes object to either clean.jsonl or excluded.jsonl.
+ *
+ * @param {object} item - associatie array with food data
+ * @returns {object} - cleaned food item
+ */
+async function cleanObj (item) {
   const obj = {
     name: item.name,
     brand: item.manufacturer,
@@ -68,51 +79,87 @@ async function cleanObj(item) {
     image: {
       sm: {
         url: item.thumbnail.url,
-        alt: item.thumbnail.name,
+        alt: item.thumbnail.name
       },
       lg: {
         url: item.image.url,
-        alt: item.image.alt,
-      },
-    },
+        alt: item.image.alt
+      }
+    }
   }
 
-  if ( item.nutritionsFactList && item.nutritionsFactList.length > 0) {
+  if (item.nutritionsFactList && item.nutritionsFactList.length > 0) {
     obj.nutrients = getNutrients(item)
-    await addToArr(obj, clean, excluded)
+    return obj
   } else {
-    await exclude(item, excluded, 'does not have a nutritionFactList')
+    await exclude(item, 'does not have a nutritionFactList')
+    return undefined
   }
 }
 
-for (const item of willys) {
-  if (excludeCategory.includes(item.category[0] ||
-    item.category[1]) ||
-    excludeCategory.includes(item.breadcrumbs[1]?.url)) {
-    continue
+/**
+ * Main function to clean the data.
+ * Iterates over the food items and saves the cleaned and complete
+ * items to clean.jsonl. Excluded items are saved to excluded.jsonl.
+ */
+async function main () {
+  const clean = [] // will contain all the complete items
+
+  for (const item of willys) {
+    if (excludeCategory.includes(item.category[0] ||
+      item.category[1]) ||
+      excludeCategory.includes(item.breadcrumbs[1]?.url)) {
+      continue
+    }
+
+    const obj = await cleanObj(item)
+    if (obj) {
+      await addToArr(obj, clean)
+    }
   }
-
-  await cleanObj(item)
+  const json = JSON.stringify(clean, null, 4)
+  await fs.writeFile('clean.json', json, 'utf8')
 }
-const json = JSON.stringify(clean, null, 4)
-await fs.writeFile('clean.json', json, 'utf8')
 
-async function exclude(obj, arr, reason) {
-  arr.push(obj)
-  console.log('item', obj.ean, obj.name, obj.manufacturer, obj.category[0], reason)
+/**
+ * Adds the excluded item to the excluded.jsonl file.
+ *
+ * @param {object} obj - the food item to be excluded
+ * @param {string} reason - the reason for exclusion
+ */
+async function exclude (obj, reason) {
+  console.error('item', obj.ean, obj.name, obj.manufacturer, obj.category[0], reason)
   await writeLine('excluded.jsonl', obj)
 }
 
-async function writeLine(filename, obj) {
+/**
+ * Appends information about a food item to a file.
+ *
+ * @param {string} filename - name of the file to append the line to.
+ * @param {object} obj - associative array with food data to be stringified and appended to file.
+ */
+async function writeLine (filename, obj) {
   await fs.appendFile(filename, JSON.stringify(obj) + '\n')
 }
 
-async function addToArr(obj, clean, excluded) {
+/**
+ * If a food item has a kcal value, it is added to the clean array.
+ * Otherwise, it is excluded and the reason is logged.
+ *
+ * @param {object} obj - the object to be added to the array
+ * @param {Array} clean - array with the clean food objects
+ */
+async function addToArr (obj, clean) {
   if (obj.nutrients.kcal_100g) {
     clean.push(obj)
     await writeLine('clean.jsonl', obj)
   } else {
-    await exclude(item, excluded, 'does not have a kcal value')
+    await exclude(obj, 'does not have a kcal value')
   }
 }
 
+try {
+  await main()
+} catch (err) {
+  console.error(err)
+}
--- data/willys.js
+++ data/willys.js
@@ -5,9 +5,10 @@
  * @version 1.1.1
  */
 
-
 import fs from 'fs/promises'
 import { JSDOM } from 'jsdom'
+
+import { parseStringPromise } from 'xml2js'
 
 const baseUrl = 'https://www.willys.se/'
 const sitemapUrl = baseUrl + 'sitemap.xml'
@@ -16,10 +17,6 @@ const apiUrl = baseUrl + 'axfood/rest/p/'
 const filename = 'willys.jsonl'
 const filename2 = 'willys.json'
 const paus = 10_000
-
-import { parseStringPromise } from 'xml2js'
-
-
 
 /**
  * Adds a paus to avoid rate limiting.
@@ -31,12 +28,16 @@ function delay (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function getProductUrl() {
+/**
+ * Gets url to the page for a single product, from the sitemap.
+ *
+ * @returns {Promise<string>} - the url to the product page
+ */
+async function getProductUrl () {
   const res = await fetch(sitemapUrl)
   const xml = await res.text()
   const parsed = await parseStringPromise(xml)
   const sitemaps = parsed.sitemapindex.sitemap
-
 
   for (const entry of sitemaps) {
     const loc = entry.loc?.[0]
@@ -63,8 +64,14 @@ export async function fetchDoc (url, config) {
   return document
 }
 
-
-async function getProductEntries(productUrl) {
+/**
+ * Gets the information about each product
+ * for all the products. Appends each product to a file.
+ *
+ * @param {string} productUrl - the url of the site that contains a list of products
+ * @returns {Promise<Array>} - the list of products
+ */
+async function getProductEntries (productUrl) {
   const res = await fetch(productUrl)
   const xml = await res.text()
   const parsed = await parseStringPromise(xml)
@@ -72,76 +79,12 @@ async function getProductEntries(productUrl) {
   const urlEntries = parsed.urlset.url
   const result = []
 
-
   for (const entry of urlEntries) {
     await delay(paus)
     try {
       const url = entry.loc[0]
-
-      const doc = await fetchDoc(url)
-      const scriptTag = doc.querySelector('script#__NEXT_DATA__')
-      const jsonText = scriptTag?.textContent
-      const data = JSON.parse(jsonText)
-
-
-      const productId = data.props.pageProps.code
-      const res = await fetch(`${apiUrl}${productId}`)
-      const json = await res.json()
-
-      const {
-        breadcrumbs,
-        ean,
-        image,
-        ingredients,
-        preparationType,
-        servingSize,
-        nutrientHeaders,
-        googleAnalyticsCategory,
-        nutritionsFactList,
-        nutrientComponents,
-        description,
-        price,
-        comparePrice,
-        comparePriceUnit,
-        manufacturer,
-        thumbnail,
-        name,
-        code
-      } = json
-
-      const obj = {
-        name,
-        code,
-        ean,
-        image: {
-          url: image.url,
-          alt: image.altText
-        },
-        category: googleAnalyticsCategory.split('|'),
-        breadcrumbs: breadcrumbs,
-        ingredients,
-        preparationType,
-        servingSize,
-        nutrientHeaders,
-        nutritionsFactList,
-        nutrientComponents,
-        description,
-        price,
-        comparePrice,
-        comparePriceUnit,
-        manufacturer,
-        thumbnail: {
-          url: thumbnail.url,
-          alt: thumbnail.altText
-        }
-      }
-
-      console.log(obj)
-      result.push(obj)
-
-        const line = JSON.stringify(obj) + '\n'
-        await fs.appendFile(filename, line)
-
+      const product = await getSingleProduct(url)
+      result.push(product)
     } catch (err) {
       console.error(err.message)
       continue
@@ -151,21 +94,95 @@ async function getProductEntries(productUrl) {
   return result
 }
 
+/**
+ * Fetches the product page and extracts the product information.
+ *
+ * @param {string} url - url to the single product page
+ * @returns {Promise<object>} - the product object
+ */
+async function getSingleProduct (url) {
+  const doc = await fetchDoc(url)
+  const scriptTag = doc.querySelector('script#__NEXT_DATA__')
+  const jsonText = scriptTag?.textContent
+  const data = JSON.parse(jsonText)
 
-async function writeToFile(filename, array) {
-  const json = JSON.stringify(array, null, 4)
+  const productId = data.props.pageProps.code
+  const res = await fetch(`${apiUrl}${productId}`)
+  const json = await res.json()
+
+  const {
+    breadcrumbs,
+    ean,
+    image,
+    ingredients,
+    preparationType,
+    servingSize,
+    nutrientHeaders,
+    googleAnalyticsCategory,
+    nutritionsFactList,
+    nutrientComponents,
+    description,
+    price,
+    comparePrice,
+    comparePriceUnit,
+    manufacturer,
+    thumbnail,
+    name,
+    code
+  } = json
+
+  const obj = {
+    name,
+    code,
+    ean,
+    image: {
+      url: image.url,
+      alt: image.altText
+    },
+    category: googleAnalyticsCategory.split('|'),
+    breadcrumbs,
+    ingredients,
+    preparationType,
+    servingSize,
+    nutrientHeaders,
+    nutritionsFactList,
+    nutrientComponents,
+    description,
+    price,
+    comparePrice,
+    comparePriceUnit,
+    manufacturer,
+    thumbnail: {
+      url: thumbnail.url,
+      alt: thumbnail.altText
+    }
+  }
+
+  console.log(obj)
+
+  const line = JSON.stringify(obj) + '\n'
+  await fs.appendFile(filename, line)
+  return obj
+}
+
+/**
+ * Writes down the food objects to a file.
+ *
+ * @param {string} filename - name of the file to write to
+ * @param {Array} arr - array with food objects to be written to the file
+ */
+async function writeToFile (filename, arr) {
+  const json = JSON.stringify(arr, null, 4)
   await fs.writeFile(filename, json, 'utf8')
 }
 
-
+/**
+ * The main function that runs the script.
+ */
 const main = async () => {
   const productUrl = await getProductUrl()
   const products = await getProductEntries(productUrl)
-  writeToFile(filename2, products)
+  await writeToFile(filename2, products)
 }
 
 await main()
-
-
-
-
