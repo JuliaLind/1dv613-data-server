8511ed074066bcdf5898842ef6301c8e92b8c897
Julia Lind <jl225vf@student.lnu.se>
2025-04-17 21:39:31 +0200

    Add another unit test

--- test/unit/models/validators.test.js
+++ test/unit/models/validators.test.js
@@ -5,7 +5,7 @@ import chai from 'chai'
 import sinon from 'sinon'
 import chaiAsPromised from 'chai-as-promised'
 import validator from 'validator'
-import { urlValidator, eanValidator } from '../../../src/models/validators.js'
+import { urlValidator, eanValidator, dateValidator } from '../../../src/models/validators.js'
 
 chai.use(chaiAsPromised)
 const expect = chai.expect
@@ -15,19 +15,30 @@ describe('validators', () => {
     sinon.restore()
   })
 
-  it('urlValidator', async function () {
-    sinon.stub(validator, 'isURL')
+  it('urlValidator, should call on the isURL method of validator node module, should return same result as the validator method', async function () {
+    sinon.stub(validator, 'isURL').returns(true)
     const url = 'myImageUrl'
-    urlValidator.validator(url)
+    const res = urlValidator.validator(url)
     expect(validator.isURL.calledOnce).to.be.true
     expect(validator.isURL.firstCall.args[0]).to.equal(url)
+    expect(res).to.be.true
   })
 
-  it('eanValidator', async function () {
-    sinon.stub(validator, 'isEAN')
+  it('eanValidator, should call on the isEAN method of validator node module, should return same result as the validator method', async function () {
+    sinon.stub(validator, 'isEAN').returns(false)
     const ean = 'someEanCode'
-    eanValidator.validator(ean)
+    const res = eanValidator.validator(ean)
     expect(validator.isEAN.calledOnce).to.be.true
     expect(validator.isEAN.firstCall.args[0]).to.equal(ean)
+    expect(res).to.be.false
+  })
+
+  it('dateValidator, should call on the isDate method of validator node module, should return same result as the validator method', async function () {
+    sinon.stub(validator, 'isDate').returns(true)
+    const date = 'somedate'
+    const res = dateValidator.validator(date)
+    expect(validator.isDate.calledOnce).to.be.true
+    expect(validator.isDate.firstCall.args[0]).to.equal(date)
+    expect(res).to.be.true
   })
 })
