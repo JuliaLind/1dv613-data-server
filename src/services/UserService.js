4082fbde218e4e2a4e7afe3050780fabc3e24fd8
Julia Lind <jl225vf@student.lnu.se>
2025-05-24 18:49:26 +0200

    Update logic to preent update of historical entries and to overwrite latest entry if date is the same

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -36,9 +36,7 @@ export class UserService {
       targetWeight,
       weeklyChange,
       activityLevel,
-      gender,
-      effectiveDate,
-      age
+      gender
     } = userData
 
     const user = new UserModel({
@@ -50,12 +48,7 @@ export class UserService {
       weeklyChange,
       activityLevel,
       history: [
-        {
-          effectiveDate,
-          currentWeight,
-          age,
-          height
-        }
+        this.#getHistoryEntry(userData)
       ]
     })
 
@@ -65,35 +58,69 @@ export class UserService {
   }
 
   /**
+   * Checks if the new user data has the latest effective date.
+   *
+   * @param {object} newData - the new user data to check
+   * @param {object} doc  - mongoose document of the user
+   * @returns {boolean} - true if the new data is the latest or same as the most recent entry in the history
+   */
+  #isLatest (newData, doc) {
+    return new Date(newData.effectiveDate) >= new Date(doc.toObject().history[0].effectiveDate)
+  }
+
+  /**
+   * Checks if there is an existing entry ith the same effective date.
+   *
+   * @param {object} newData - the new user data to check
+   * @param {object} doc  - mongoose document of the user
+   * @returns {boolean} - true if there already is a history entry with the same effective date
+   */
+  #entryExists (newData, doc) {
+    return doc.history.some(entry => entry.effectiveDate === newData.effectiveDate)
+  }
+
+  /**
+   * Puts together a new history entry from the new data.
+   *
+   * @param {object} newData - the new user data to create a history entry from
+   * @returns {object} - the history entry object to be stored in the database
+   */
+  #getHistoryEntry (newData) {
+    const {
+      effectiveDate,
+      currentWeight,
+      age,
+      height
+    } = newData
+
+    return {
+      effectiveDate,
+      currentWeight,
+      age,
+      height
+    }
+  }
+
+  /**
    * Updates the user data document in the database.
    *
    * @param {object} doc - The user document to update.
    * @param {object} newData - The new data to update the user document with.
    */
   async upd (doc, newData) {
-    doc.set(newData)
-    const {
-      height,
-      currentWeight,
-      effectiveDate,
-      age
-    } = newData
+    if (!this.#isLatest(newData, doc)) {
+      throw createError(400, 'Cannot update user history')
+    }
 
-    if (doc.history.length > 0 && doc.history[0].effectiveDate === newData.effectiveDate) {
+    doc.set(newData)
+
+    const historyEntry = this.#getHistoryEntry(newData)
+
+    if (this.#entryExists(newData, doc)) {
       // replace existing entry if the effective date is the same
-      doc.history[0] = {
-        effectiveDate,
-        currentWeight,
-        age,
-        height
-      }
+      doc.history[0] = historyEntry
     } else {
-      doc.history.unshift({
-        effectiveDate,
-        currentWeight,
-        age,
-        height
-      })
+      doc.history.unshift(historyEntry)
     }
 
     if (doc.isModified()) {
