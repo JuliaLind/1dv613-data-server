d47755630f5354590c72d74751461c2a910a5fba
Julia Lind <jl225vf@student.lnu.se>
2025-06-01 08:08:47 +0200

    Allow larger files

--- src/config/create-app.js
+++ src/config/create-app.js
@@ -24,7 +24,7 @@ export function createApp () {
 
   // use helmet for security
   app.use(helmet())
-  app.use(express.json())
+  app.use(express.json({ limit: '500kb' }))
   // Enable Cross Origin Resource Sharing (CORS) (https://www.npmjs.com/package/cors).
   app.use(cors())
   app.use(logger('dev'))
